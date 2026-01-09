02ba32425ceb998bee9d0dacfc06af81a0726ee9
diegomadera1612-oss <diegomadera1612@gmail.com>
2026-01-09 07:45:06 -0500

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-mySprite = sprites.create(assets.image("""
+def on_a_pressed():
+    pass
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+
+Mau = sprites.create(assets.image("""
     Mau
     """), SpriteKind.player)
+
+music.play("Kanye West - Flashing Lights ft. Dwele 4.mp3", music.PlaybackMode.UNTIL_DONE)
--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+controller.A.onEvent(ControllerButtonEvent.Pressed, function on_a_pressed() {
+    
 })
-let Mau = sprites.create(assets.image`Mau`, SpriteKind.Player)
+let Mau = sprites.create(assets.image`
+    Mau
+    `, SpriteKind.Player)
+music.play(music.createSong(null), music.PlaybackMode.UntilDone)
